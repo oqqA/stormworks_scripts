5303a293c6e286016595ee0d737bea56e7e48114
oqqA <oqqa.vw@gmail.com>
2020-08-30 15:06:24 +0400

    refactor

--- aircraft-helicopter.lua
+++ aircraft-helicopter.lua
@@ -12,7 +12,9 @@ function pid(p,i,d)
 	}
 end
 
-function getN(a,b)local x={} for v=a,b do table.insert(x, input.getNumber(v))end;return table.unpack(x)end
+--function gj(flag,a,b) local x,fF={},input.getNumber if flag then fF = input.getBool end; for v=a,b do table.insert(x, fF(v))end;return table.unpack(x) end
+function getB(a,b) local x={} for v=a,b do table.insert(x, input.getBool(v))end;return table.unpack(x) end
+function getN(a,b) local x={} for v=a,b do table.insert(x, input.getNumber(v))end;return table.unpack(x)end
 function outN(o, ...) for i,v in pairs({...}) do output.setNumber(o+i-1,v) end end
 
 --function getB(...)local a={}for b,c in ipairs({...})do a[b]=input.getBool(c)end;return tU(a)end
@@ -25,6 +27,14 @@ function outN(o, ...) for i,v in pairs({...}) do output.setNumber(o+i-1,v) end e
 -- r - rotate
 -- i - input
 
+height = 8.22 + 6
+
+
+vLateral, vForward, vVertical, tPitch, tRoll, altimeter, vAngular, altL, altR = 0
+iAD, iWS, iLR, iUD = 0
+paL,paR,paB,rL,rR,rB = 0
+
+isModeList = {} -- 1 - helicopter, 2 - aircraft, 3 - altitudeHold,  4 - autopilot, 
 
 pid1 = pid(0.1, 0, 4)
 pid2 = pid(0.1, 0, 2)
@@ -36,62 +46,75 @@ pidL = pid(0.1, 0, 4)
 pidR = pid(0.1, 0, 4)
 pidB = pid(0.1, 0.001, 4)
 
-height = 8.22 + 6
 
+function helicopterMode()
+	--tempVal1 = (-vForward)*4 + tRoll*8 --edited
+	--tempVal2 = pidL:run(height,altimeter)
+	tempVal3 = pid3:run(0,vAngular)
+	
+	tempValL = pidL:run(0,altL - height)
+	tempValR = pidR:run(0,altR - height)
+	
+	--if vForward < 0 then temptempVal = -temptempVal end
+		
+	tempValBv = vForward
+		
+	if vForward < -0.5 then vForward = -0.5 end
+	if 0.5 > vForward then  vForward = 0.5 end
+		
+	tempValB = pidB:run(0, vForward/50 + tPitch*8)
+
+	paL = tempValL*0.4 - vLateral/100 + iLR/10 -- + tRoll*4 
+	paR = tempValR*0.4 + vLateral/100 - iLR/10 -- - tRoll*4
+	paB = tempValB + iUD/10 -- -vForward -- /4
+	
+	rL = -tempVal3 - iAD/2
+	rR = tempVal3 + iAD/2
+	rB = 0
+end
+
+function aircraftMode()
+
+	paL = 1 -- + 0.14
+    paR = 1 -- + 0.14
+    paB = -0.25 -- -1
+
+    rL = -1 + iLR/20
+    rR = -1 - iLR/20
+    rB = -1
+
+    height = altimeter + 10
+end
+
+function altitudeHoldMode()
+end
+
+function autopilotMode()
+end
+
+modes = {helicopterMode, aircraftMode, altitudeHoldMode, autopilotMode}
+
+lastMode = 0
+activeTransitionMode = 0
 
 function onTick()
-	-- input
+    -- input
+    
 	vLateral, vForward, vVertical, tPitch, tRoll, altimeter, vAngular, altL, altR = getN(1,9)
 	iAD, iWS, iLR, iUD = getN(10,15)
-	
-	height = height + iWS/10
-	
-	mod = input.getBool(2)
-	
-	-- act
-	
-	
-	tick = 0
-	
-	if mod then
-		
-		paL = 0.14 + 1
-		paR = 0.14 + 1
-		paB = 0.15
-	
-		rL = -1 + iLR/20
-		rR = -1 - iLR/20
-		rB = 0
-		
-		height = altimeter + 15
-    else
-        --tempVal1 = (-vForward)*4 + tRoll*8 --edited
-	    --tempVal2 = pidL:run(height,altimeter)
-
-        tempVal3 = pid3:run(0,vAngular)
-	
-			
-        tempValL = pidL:run(0,altL - height)
-        tempValR = pidR:run(0,altR - height)
-        
-        --if vForward < 0 then temptempVal = -temptempVal end
-            
-        tempValBv = vForward
-            
-        if vForward < -0.5 then vForward = -0.5 end
-        if 0.5 > vForward then  vForward = 0.5 end
-            
-        tempValB = pidB:run(0, vForward/50 + tPitch*8)
+    isModeList = table.pack(true,getB(2,4))
     
-        paL = tempValL*0.4 - vLateral/100 + iLR/10 -- + tRoll*4 
-        paR = tempValR*0.4 + vLateral/100 - iLR/10 -- - tRoll*4
-        paB = tempValB + iUD/10 -- -vForward -- /4
-        
-        rL = -tempVal3 - iAD/2
-        rR = tempVal3 + iAD/2
-        rB = 0
-	end
-	
+    height = height + iWS/10
+
+    -- act
+
+    for i = 4, 1, -1 do 
+        if isModeList[i] then
+            modes[i]()
+            break
+        end
+    end
+    
 	-- output
 
 	outN(1,paL,paR,paB,rL,rR,rB,-iWS,iAD)
