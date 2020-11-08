7c84704f9a8bae1b36392bf0833e847559b1c8e8
oqqA <oqqa.vw@gmail.com>
2020-11-08 20:27:03 +0400

    fix: correct Z coordinate

--- track_player_2_radar.lua
+++ track_player_2_radar.lua
@@ -1,15 +1,16 @@
 -- not ready
--- bug: detect legs, torso, head
 
+function equal(a,b) return math.abs(a-b)<0.1 end
 function getN(o,s) local x={} for v=o,o+s do table.insert(x, input.getNumber(v))end;return table.unpack(x)end
 function outN(o,...) for i,v in ipairs({...}) do output.setNumber(o+i-1,v) end end
 
 delayTick = 4
 rP = { {3,0}, {0,0} } -- radarsPoz
 d = math.sqrt( (rP[2][1]-rP[1][1])^2 + (rP[2][2]-rP[1][2])^2 )
+radarBelowOrAbove = property.getNumber("radar up or down")
 
 STEP, mode, tick = 0.01, 1, 0
-aL, aR, altL, altR, dL, dR = 0, 0, 0, 0, 0, 0
+aL, aR, altL, altR, dL, dR, stL, stR = 0, 0, 0, 0, 0, 0, 0, 0
 minL, minR = {-0.5, 3000, 0}, {-0.5, 3000, 0}
 x, y, z = 0, 0, 0
 
@@ -18,11 +19,11 @@ function find()
     aL, aR = ang, ang
 
     if dL and altL and dL ~= 0 and dL < minL[2] then
-        minL = {-0.5+(tick-delayTick)*STEP, dL, altL}
+        minL = {-0.5+(tick-delayTick)*STEP, dL, altL, stL*dL}
     end
 
     if dR and altR and dR ~= 0 and dR < minR[2] then
-        minR = {-0.5+(tick-delayTick)*STEP, dR, altR}
+        minR = {-0.5+(tick-delayTick)*STEP, dR, altR, stR*dR}
     end
 
     if ang > 0.5+delayTick*STEP then
@@ -41,21 +42,25 @@ function track()
     ddL = minL[2] * math.cos(minL[3]*2*math.pi)
     ddR = minR[2] * math.cos(minR[3]*2*math.pi)
 
-    a = ( ddL^2 - ddR^2 + d^2 ) / ( 2 * d )
-    h = math.sqrt( ddR^2 - a^2 )
-    
-    x,y,z = ddL*4, ddR*4, minR[3]
+    a = ( minL[2]^2 - minR[2]^2 + d^2 ) / ( 2 * d )
+    h = math.sqrt( minR[2]^2 - a^2 )
 
     x2 = rP[2][1] + a * ( rP[2][1] - rP[1][1] ) / d
     y2 = rP[2][2] + a * ( rP[2][2] - rP[1][2] ) / d
 
-    --x = x2 + h * ( rP[2][2] - rP[1][2] ) / d
-    --y = y2 - h * ( rP[2][1] - rP[1][1] ) / d
-    --x, y = x*4, y*4
+    x = ( x2 + h * ( rP[2][2] - rP[1][2] ) / d ) * 4
+    y = ( y2 - h * ( rP[2][1] - rP[1][1] ) / d ) * 4
+    z = minR[2] * math.sin(minR[3]*2*math.pi) * 4
 
+        --x,y = ddL*4, ddR*4
         --x = minL[2] * math.cos(minL[3]*2*math.pi) * 4
         --y = minR[2] * math.cos(minR[3]*2*math.pi) * 4
-    z = ddR * math.sin(minR[3]*2*math.pi) * 4
+    
+    if equal(25,minR[4]) and radarBelowOrAbove == 0 then z=z-0.05199+5.12 end
+    if equal(12.5,minR[4]) and radarBelowOrAbove == 0 then z=z-0.81188+5.12 end
+
+    if equal(25,minR[4]) and radarBelowOrAbove == 1 then z=z+5.068 end
+    if equal(12.5,minR[4]) and radarBelowOrAbove == 1 then z=z+2.508 end
 
     minL, minR = {-0.5, 3000, 0}, {-0.5, 3000, 0}
     mode = 1
@@ -67,10 +72,17 @@ end
 modes = { find, track }
 
 function onTick()
-    dL, dR, altL, altR = getN(1,4)
+    dL, dR, altL, altR, stL, stR = getN(1,6)
 
     modes[mode]()
 
     outN(1, x, y, z)
-    outN(10, aL, aR)
+    outN(10, aL, aR, 0.125)
 end
+
+
+
+-- можно улучшить нахождением угла противолежащего катета (n-x)
+-- или найти прямоугольный треугольник к игроку
+ 
+-- и убрать 2 входа strength сигнала и property
