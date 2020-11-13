83eeb7f2b1b35779c32f66d3b630d1f509042c40
oqqA <oqqa.vw@gmail.com>
2020-11-13 17:42:00 +0400

    add: new function

--- help-functions.lua
+++ help-functions.lua
@@ -9,28 +9,4 @@ function pid(p,i,d,ci)return{p=p,i=i,d=d,ci=ci,E=0,D=0,I=0,run=function(s,sp,pv)
 function loop(n,maxN) return (n-maxN) % (maxN*2) - maxN end -- use +-math.pi or +-0.5
 
 function gammaFix(v) local a,y=1.0,2.2 for i=1, #v do v[i]=((a*v[i])^y)/(255^y)*v[i] end return v end -- for real color block use: a,y=0.85,2.4
-
------- for generate rainbow 
-function h2rgb(m1, m2, h)
-	if h<0 then h = h+1 end
-	if h>1 then h = h-1 end
-	if h*6<1 then
-		return m1+(m2-m1)*h*6
-	elseif h*2<1 then
-		return m2
-	elseif h*3<2 then
-		return m1+(m2-m1)*(2/3-h)*6
-	else
-		return m1
-	end
-end
-
-function hsl_to_rgb(h, s, L)
-	local m2 = L <= .5 and L*(s+1) or L+s-L*s
-	local m1 = L*2-m2
-	return
-		h2rgb(m1, m2, h+1/3),
-		h2rgb(m1, m2, h),
-		h2rgb(m1, m2, h-1/3)
-end
-------
+function hex2rgb(h) local h = h:gsub("#","") return tonumber(h:sub(1,2),16), tonumber(h:sub(3,4),16), tonumber(h:sub(5,6),16) end
