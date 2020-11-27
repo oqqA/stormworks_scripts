e3789682698f1a48de05d3cb3643e32838665959
oqqA <oqqa.vw@gmail.com>
2020-11-28 01:21:47 +0400

    fix: add gap draw

--- monitoring_stand_3x5.lua
+++ monitoring_stand_3x5.lua
@@ -1,12 +1,11 @@
 MAX_INPUT = 3
 SIZE_BUF = 600
 SCALE_TICK = 160-21
+GAP = 2
 
 function getN(o,s) local x={} for v=o,o+s do x[v]=input.getNumber(v) end;return x end
 function getB(o,s) local x={} for v=o,o+s do x[v]=input.getBool(v) end;return x end
 function hex2rgb(h) local h = h:gsub("#","") return {tonumber(h:sub(1,2),16), tonumber(h:sub(3,4),16), tonumber(h:sub(5,6),16)} end
-
-tick = 0
 
 function graph(color)
     return {
@@ -23,28 +22,34 @@ function graph(color)
         draw=function(s,startTick, scaleTick, xOffset, yOffset, xScale, yScale, width, height)
             if #s.data < 2 then return end
             local lastV = { startTick, s.lastN }
+            local gapLastV = lastV
             screen.setColor(table.unpack(color))
             for _,v in ipairs(s.data) do
                 v = {v[1], lastV[2] + v[2]}
-                screen.drawLine(
-                    width-(startTick-lastV[1])*xScale + xOffset,
-                    height/2-(lastV[2])*yScale + yOffset,
-                    width-(startTick-v[1])*xScale + xOffset,
-                    height/2-(v[2])*yScale + yOffset ) 
+                
+                if ( (scaleTick*xScale+1) * v[1] ) % GAP == 0 then 
+	                screen.drawLine(
+	                    width-(startTick-gapLastV[1])*xScale + xOffset,
+	                    height/2-(gapLastV[2])*yScale + yOffset,
+	                    width-(startTick-v[1])*xScale + xOffset,
+	                    height/2-(v[2])*yScale + yOffset ) 
+	                gapLastV = v
+                end
+                
                 lastV = v
-                if v[1] < startTick - (scaleTick)/xScale then break end
+                if v[1] < startTick - (scaleTick + GAP)/xScale then break end
             end
         end
     }
 end
 
+tick = 0
 rainbow10colors = {'#C04040', '#C08D40','#A6C040','#5AC040','#40C073','#40C0C0','#4073C0','#5A40C0','#A640C0','#C0408D'}
 --rainbow5colors = { '#FF0000','#CCFF00','#00FF67','#0066FF','#CC00FF' }
 graphs={}
 g1 = graph(hex2rgb('#00ff00'))
 sw, sh = 0, 0
 scale = { 0, 0 }
-
 
 function onTick()
     data = getN(1,MAX_INPUT-1)
@@ -65,7 +70,7 @@ function onDraw()
     
     screen.setColor(table.unpack(hex2rgb('#FFFFFF')))
 	
-	screen.drawText(sw-20,1,'14.3')
+	screen.drawText(sw-20,1,'3.14')
 	screen.drawText(sw-20,7,'kek')
 	screen.drawText(sw-20,sh-6,'kek')
 	
@@ -75,6 +80,6 @@ function onDraw()
 	
     for _,v in pairs(graphs) do
         v:draw(tick, SCALE_TICK, -21, 3, scale[1], scale[2], sw, sh)
-        v:draw(tick, (160-21) , -21, 3, 0.25, 6, sw, 8)
+        v:draw(tick, (sw-21) , -21, 3, 0.25, 6, sw, 8)
     end
 end
