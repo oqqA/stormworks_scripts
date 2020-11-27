824fb0cf7ac105bea3bd432936e0da6466164a12
oqqA <oqqa.vw@gmail.com>
2020-11-28 02:05:30 +0400

    refact: argupents pack to table

--- monitoring_stand_3x5.lua
+++ monitoring_stand_3x5.lua
@@ -2,6 +2,8 @@ MAX_INPUT = 3
 SIZE_BUF = 600
 SCALE_TICK = 160-21
 GAP = 2
+RIGHT_INDENT = 21
+TOP_INDENT = 12
 
 function getN(o,s) local x={} for v=o,o+s do x[v]=input.getNumber(v) end;return x end
 function getB(o,s) local x={} for v=o,o+s do x[v]=input.getBool(v) end;return x end
@@ -19,7 +21,7 @@ function graph(color)
             s.lastN = n
             if #s.data > SIZE_BUF then table.remove(s.data) end
         end,
-        draw=function(s,startTick, scaleTick, xOffset, yOffset, xScale, yScale, width, height)
+        draw=function(s,startTick, scaleTick, offset, scale, wh)
             if #s.data < 2 then return end
             local lastV = { startTick, s.lastN }
             local gapLastV = lastV
@@ -27,29 +29,31 @@ function graph(color)
             for _,v in ipairs(s.data) do
                 v = {v[1], lastV[2] + v[2]}
                 
-                if ( (scaleTick*xScale+1) * v[1] ) % GAP == 0 then 
+                if ( (scaleTick*scale[1]+1) * v[1] ) % GAP == 0 then 
 	                screen.drawLine(
-	                    width-(startTick-gapLastV[1])*xScale + xOffset,
-	                    height/2-(gapLastV[2])*yScale + yOffset,
-	                    width-(startTick-v[1])*xScale + xOffset,
-	                    height/2-(v[2])*yScale + yOffset ) 
+	                    wh[1] - (startTick-gapLastV[1])*scale[1] + offset[1],
+	                    wh[2]/2-(gapLastV[2])*scale[2] + offset[2],
+	                    wh[1] - (startTick-v[1])*scale[1] + offset[1],
+	                    wh[2]/2-(v[2])*scale[2] + offset[2] ) 
 	                gapLastV = v
                 end
                 
                 lastV = v
-                if v[1] < startTick - (scaleTick + GAP)/xScale then break end
+                if v[1] < startTick - (scaleTick + GAP)/scale[1] then break end
             end
         end
     }
 end
 
-tick = 0
+
 rainbow10colors = {'#C04040', '#C08D40','#A6C040','#5AC040','#40C073','#40C0C0','#4073C0','#5A40C0','#A640C0','#C0408D'}
 --rainbow5colors = { '#FF0000','#CCFF00','#00FF67','#0066FF','#CC00FF' }
-graphs={}
-g1 = graph(hex2rgb('#00ff00'))
-sw, sh = 0, 0
+
+wh = { 0, 0 }
 scale = { 0, 0 }
+indents = { RIGHT_INDENT, TOP_INDENT }
+graphs={}
+tick = 0
 
 function onTick()
     data = getN(1,MAX_INPUT-1)
@@ -63,23 +67,22 @@ end
 flag = true
 function onDraw()
     if flag then
-        sw, sh = screen.getWidth(), screen.getHeight()
-        scale = { sw/SCALE_TICK, sh/2-12 }
+        wh = { screen.getWidth(), screen.getHeight() }
+        scale = { wh[1]/(wh[1]-indents[1]), wh[2]/2-indents[2] }
         flag = false
     end
     
     screen.setColor(table.unpack(hex2rgb('#FFFFFF')))
 	
-	screen.drawText(sw-20,1,'3.14')
-	screen.drawText(sw-20,7,'kek')
-	screen.drawText(sw-20,sh-6,'kek')
+	screen.drawText(wh[1]-20,1,'3.14')
+	screen.drawText(wh[1]-20,7,'kek')
+	screen.drawText(wh[1]-20,wh[2]-6,'kek')
 	
-	screen.drawLine(0,14,sw,14)
-	screen.drawLine(sw-21,0,sw-21,sh)
-	
-	
+	screen.drawLine(0,14,wh[1],14)
+	screen.drawLine(wh[1]-21,0,wh[1]-21,wh[2])
+
     for _,v in pairs(graphs) do
-        v:draw(tick, SCALE_TICK, -21, 3, scale[1], scale[2], sw, sh)
-        v:draw(tick, (sw-21) , -21, 3, 0.25, 6, sw, 8)
+        v:draw(tick, (wh[1]-indents[1]), {-indents[1], 3}, scale, wh)
+        v:draw(tick, (wh[1]-indents[1]), {-indents[1], 3}, {0.25, 6}, {wh[1], 8})
     end
 end
